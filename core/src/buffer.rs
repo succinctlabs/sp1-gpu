15313ccc06670af9cb0c3aeb4ea88d3388649e1e
tamirhemo <tamirhmo@gmai.com>
2024-05-15 20:03:56 +0000

    add tests

--- core/src/buffer.rs
+++ core/src/buffer.rs
@@ -1,4 +1,11 @@
-use crate::mem::{copy_host_to_device, cuda_free, cuda_malloc};
+use crate::mem::{copy_device_to_host, copy_host_to_device, cuda_free, cuda_malloc};
+
+use std::ops::{
+    Index, IndexMut, Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive,
+};
+use std::slice;
+
+use crate::slice::DeviceSlice;
 
 /// Fixed-size device-side buffer.
 #[derive(Debug)]
@@ -33,6 +40,14 @@ impl<T: Copy> DeviceBuffer<T> {
         self.len == 0
     }
 
+    pub fn as_slice(&self) -> &DeviceSlice<T> {
+        &self[..]
+    }
+
+    pub fn as_slice_mut(&mut self) -> &mut DeviceSlice<T> {
+        &mut self[..]
+    }
+
     /// Copies all elements from `src` into `self`, using a cudaMemcpy.
     ///
     /// The length of `src` must be the same as `self`.
@@ -41,7 +56,7 @@ impl<T: Copy> DeviceBuffer<T> {
     ///
     /// This function will panic if the two slices have different lengths or if cudaMalloc
     /// returned an error.
-    pub fn copy_from_slice(&mut self, src: &[T]) {
+    pub fn copy_from_host(&mut self, src: &[T]) {
         // The panic code path was put into a cold function to not bloat the
         // call site.
         #[inline(never)]
@@ -61,6 +76,26 @@ impl<T: Copy> DeviceBuffer<T> {
         unsafe { copy_host_to_device(self.buf, src.as_ptr(), src.len()) }.unwrap()
     }
 
+    pub fn copy_into_host(&self, dst: &mut [T]) {
+        // The panic code path was put into a cold function to not bloat the
+        // call site.
+        #[inline(never)]
+        #[cold]
+        #[track_caller]
+        fn len_mismatch_fail(dst_len: usize, src_len: usize) -> ! {
+            panic!(
+                "source slice length ({}) does not match destination slice length ({})",
+                src_len, dst_len,
+            );
+        }
+
+        if self.len() != dst.len() {
+            len_mismatch_fail(self.len(), dst.len());
+        }
+
+        unsafe { copy_device_to_host(dst.as_mut_ptr(), self.buf, dst.len()) }.unwrap()
+    }
+
     /// Calculates the offset to the current element.
     #[inline]
     unsafe fn offset(&self) -> *mut T {
@@ -73,7 +108,7 @@ impl<T: Copy> DeviceBuffer<T> {
     ///
     /// This function will panic if the resulting length will extend the buffer's capacity or if
     /// cudaMalloc returned an error.
-    pub fn extend_from_slice(&mut self, src: &[T]) {
+    pub fn extend_from_host_slice(&mut self, src: &[T]) {
         // The panic code path was put into a cold function to not bloat the
         // call site.
         #[inline(never)]
@@ -90,12 +125,130 @@ impl<T: Copy> DeviceBuffer<T> {
             capacity_fail(self.len(), src.len(), self.cap);
         }
 
-        unsafe { copy_host_to_device(self.offset(), src.as_ptr(), src.len()) }.unwrap()
+        unsafe { copy_host_to_device(self.offset(), src.as_ptr(), src.len()) }.unwrap();
+
+        // Extend the length of the buffer to include the new elements.
+        self.len += src.len();
     }
 }
 
 impl<T: Copy> Drop for DeviceBuffer<T> {
     fn drop(&mut self) {
         unsafe { cuda_free(self.buf) }.unwrap()
+    }
+}
+
+macro_rules! impl_index {
+    ($($t:ty)*) => {
+        $(
+            impl<T : Copy> Index<$t> for DeviceBuffer<T>
+            {
+                type Output = DeviceSlice<T>;
+
+                fn index(&self, index: $t) -> &DeviceSlice<T> {
+                    unsafe {
+                        DeviceSlice::from_slice(
+                         slice::from_raw_parts(self.buf, self.len).index(index)
+                    )
+                  }
+                }
+            }
+
+            impl<T : Copy> IndexMut<$t> for DeviceBuffer<T>
+            {
+                fn index_mut(&mut self, index: $t) -> &mut DeviceSlice<T> {
+                    unsafe {
+                        DeviceSlice::from_slice_mut(
+                            slice::from_raw_parts_mut(self.buf, self.len).index_mut(index)
+                        )
+                    }
+                }
+            }
+        )*
+    }
+}
+
+impl_index! {
+    Range<usize>
+    RangeFull
+    RangeFrom<usize>
+    RangeInclusive<usize>
+    RangeTo<usize>
+    RangeToInclusive<usize>
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::{
+        distributions::{Distribution, Standard},
+        thread_rng, Rng,
+    };
+    use std::{fmt::Debug, ops::Range};
+
+    use super::DeviceBuffer;
+
+    fn make_test_buffer_init_and_copy<T>(rng: &mut impl Rng, len: usize)
+    where
+        T: Debug + Copy + Default + Eq,
+        Standard: Distribution<T>,
+    {
+        let mut buffer = DeviceBuffer::<T>::new(len);
+        assert_eq!(buffer.len(), 0);
+
+        let values = (0..len).map(|_| rng.gen()).collect::<Vec<_>>();
+        buffer.extend_from_host_slice(&values);
+        assert_eq!(buffer.len(), values.len());
+
+        let mut values_back = vec![T::default(); len];
+        buffer.copy_into_host(&mut values_back);
+
+        for (val, exp) in values_back.into_iter().zip(values) {
+            assert_eq!(val, exp);
+        }
+    }
+
+    fn make_test_buffer_slice_index<T>(rng: &mut impl Rng, len: usize, slice_range: Range<usize>)
+    where
+        T: Debug + Copy + Default + Eq,
+        Standard: Distribution<T>,
+    {
+        let mut buffer = DeviceBuffer::<T>::new(len);
+        assert_eq!(buffer.len(), 0);
+
+        // Initialize the buffer to zero.
+        buffer.extend_from_host_slice(&vec![T::default(); len]);
+        assert_eq!(buffer.len(), len);
+
+        let new_values = slice_range.clone().map(|_| rng.gen()).collect::<Vec<_>>();
+        let device_slice = &mut buffer[slice_range.clone()];
+        assert_eq!(device_slice.len(), slice_range.len());
+
+        device_slice.copy_from_host(&new_values);
+
+        let mut new_values_back = vec![T::default(); len];
+        device_slice.copy_into_host(&mut new_values_back[0..slice_range.len()]);
+
+        for (val, exp) in new_values_back.into_iter().zip(new_values) {
+            assert_eq!(val, exp);
+        }
+    }
+
+    #[test]
+    fn test_buffer_init_and_copy() {
+        let len = 10000;
+
+        let mut rng = thread_rng();
+        make_test_buffer_init_and_copy::<u32>(&mut rng, len);
+        make_test_buffer_init_and_copy::<u64>(&mut rng, len);
+    }
+
+    #[test]
+    fn test_buffer_slice_index() {
+        let len = 10000;
+        let range = 34..900;
+
+        let mut rng = thread_rng();
+        make_test_buffer_slice_index::<u32>(&mut rng, len, range.clone());
+        make_test_buffer_slice_index::<u64>(&mut rng, len, range);
     }
 }
--- core/src/slice.rs
+++ core/src/slice.rs
@@ -1,37 +1,53 @@
-use std::marker::PhantomData;
+use core::slice;
+use std::ops::{
+    Index, IndexMut, Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive,
+};
 
-use crate::{error::CudaError, mem::copy_host_to_device};
+use crate::mem::{copy_device_to_host, copy_host_to_device};
 
-/// Fixed-size device-side buffer.
 #[derive(Debug)]
-#[repr(C)]
-pub struct DeviceSlice<T: Copy> {
-    ptr: *mut T,
-    len: usize,
-    _marker: PhantomData<[T]>,
-}
-
-unsafe impl<T: Sync + Copy> Sync for DeviceSlice<T> {}
+#[repr(transparent)]
+pub struct DeviceSlice<T>([T]);
 
 impl<T: Copy> DeviceSlice<T> {
     #[inline]
     pub const fn len(&self) -> usize {
-        self.len
+        self.0.len()
     }
 
     #[inline]
     pub const fn is_empty(&self) -> bool {
-        self.len == 0
+        self.0.is_empty()
     }
 
     #[inline]
-    pub const fn as_ptr(&self) -> *const T {
-        self.ptr
+    pub fn as_ptr(&self) -> *const T {
+        self.0.as_ptr()
     }
 
     #[inline]
     pub fn as_mut_ptr(&mut self) -> *mut T {
-        self.ptr
+        self.0.as_mut_ptr()
+    }
+
+    #[inline(always)]
+    pub(crate) unsafe fn from_slice(src: &[T]) -> &Self {
+        &*(src as *const [T] as *const Self)
+    }
+
+    #[inline(always)]
+    pub(crate) unsafe fn from_slice_mut(src: &mut [T]) -> &mut Self {
+        &mut *(src as *mut [T] as *mut Self)
+    }
+
+    /// # Safety
+    pub unsafe fn from_raw_parts<'a>(data: *const T, len: usize) -> &'a Self {
+        Self::from_slice(slice::from_raw_parts(data, len))
+    }
+
+    /// # Safety
+    pub unsafe fn from_raw_parts_mut<'a>(data: *mut T, len: usize) -> &'a mut Self {
+        Self::from_slice_mut(slice::from_raw_parts_mut(data, len))
     }
 
     /// Copies all elements from `src` into `self`, using a cudaMemcpy.
@@ -40,9 +56,9 @@ impl<T: Copy> DeviceSlice<T> {
     ///
     /// # Panics
     ///
-    /// This function will panic if the two slices have different lengths.
-    #[inline]
-    pub fn copy_from_slice(&mut self, src: &[T]) -> Result<(), CudaError> {
+    /// This function will panic if the two slices have different lengths or if cudaMalloc
+    /// returned an error.
+    pub fn copy_from_host(&mut self, src: &[T]) {
         // The panic code path was put into a cold function to not bloat the
         // call site.
         #[inline(never)]
@@ -59,6 +75,65 @@ impl<T: Copy> DeviceSlice<T> {
             len_mismatch_fail(self.len(), src.len());
         }
 
-        unsafe { copy_host_to_device(self.ptr, src.as_ptr(), src.len()) }
+        unsafe { copy_host_to_device(self.0.as_mut_ptr(), src.as_ptr(), src.len()) }.unwrap()
     }
+
+    /// Copies all elements from `src` into `self`, using a cudaMemcpy.
+    ///
+    /// The length of `src` must be the same as `self`.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the two slices have different lengths or if cudaMalloc
+    /// returned an error.
+    pub fn copy_into_host(&self, dst: &mut [T]) {
+        // The panic code path was put into a cold function to not bloat the
+        // call site.
+        #[inline(never)]
+        #[cold]
+        #[track_caller]
+        fn len_mismatch_fail(dst_len: usize, src_len: usize) -> ! {
+            panic!(
+                "source slice length ({}) does not match destination slice length ({})",
+                src_len, dst_len,
+            );
+        }
+
+        if self.len() != dst.len() {
+            len_mismatch_fail(self.len(), dst.len());
+        }
+
+        unsafe { copy_device_to_host(dst.as_mut_ptr(), self.0.as_ptr(), dst.len()) }.unwrap()
+    }
+}
+
+macro_rules! impl_index {
+    ($($t:ty)*) => {
+        $(
+            impl<T : Copy> Index<$t> for DeviceSlice<T>
+            {
+                type Output = DeviceSlice<T>;
+
+                fn index(&self, index: $t) -> &Self {
+                    unsafe { DeviceSlice::from_slice(self.0.index(index)) }
+                }
+            }
+
+            impl<T : Copy> IndexMut<$t> for DeviceSlice<T>
+            {
+                fn index_mut(&mut self, index: $t) -> &mut Self {
+                    unsafe { DeviceSlice::from_slice_mut( self.0.index_mut(index)) }
+                }
+            }
+        )*
+    }
+}
+
+impl_index! {
+    Range<usize>
+    RangeFull
+    RangeFrom<usize>
+    RangeInclusive<usize>
+    RangeTo<usize>
+    RangeToInclusive<usize>
 }
