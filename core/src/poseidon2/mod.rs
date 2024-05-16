4b76b22ca8cba278010bb67433adb4112af3bd7d
John Guibas <jtguibas@gmail.com>
2024-05-16 04:31:56 +0000

    p2 suite

--- core/src/poseidon2/mod.rs
+++ core/src/poseidon2/mod.rs
@@ -8,6 +8,7 @@ pub mod poseidon2_bb31_16_gpu {
     pub const ROUNDS_F: usize = 8;
     pub const ROUNDS_P: usize = 13;
     pub const WIDTH: usize = 16;
+    pub const RATE: usize = 8;
     pub const DIGEST_WIDTH: usize = 8;
     pub const D_U64: u64 = 7;
 
@@ -34,6 +35,17 @@ pub mod poseidon2_bb31_16_gpu {
             n_blocks: usize,
             n_threads_per_block: usize,
         );
+
+        pub fn hash(
+            input: DeviceSliceRaw<BabyBear>,
+            n_input: usize,
+            output: DeviceSliceRaw<[BabyBear; DIGEST_WIDTH]>,
+            external_rc: DeviceSliceRaw<[BabyBear; WIDTH]>,
+            internal_rc: DeviceSliceRaw<BabyBear>,
+            n: usize,
+            n_blocks: usize,
+            n_threads_per_block: usize,
+        );
     }
 }
 
@@ -47,6 +59,8 @@ mod tests {
     use p3_field::AbstractField;
     use p3_poseidon2::Poseidon2;
     use p3_poseidon2::Poseidon2ExternalMatrixGeneral;
+    use p3_symmetric::CryptographicHasher;
+    use p3_symmetric::PaddingFreeSponge;
     use p3_symmetric::Permutation;
     use rand::thread_rng;
     use rand::Rng;
@@ -54,6 +68,7 @@ mod tests {
     use super::poseidon2_bb31_16_gpu;
     use super::poseidon2_bb31_16_gpu::DIGEST_WIDTH;
     use super::poseidon2_bb31_16_gpu::D_U64;
+    use super::poseidon2_bb31_16_gpu::RATE;
     use super::poseidon2_bb31_16_gpu::ROUNDS_F;
     use super::poseidon2_bb31_16_gpu::ROUNDS_P;
     use super::poseidon2_bb31_16_gpu::WIDTH;
@@ -140,8 +155,8 @@ mod tests {
 
         // Copy the result of the kernel to the host.
         output_device.copy_to_host(&mut output[..]);
-        for (a, b) in gt.iter().zip(output.iter()) {
-            assert_eq!(*a, *b);
+        for i in 0..n {
+            assert_eq!(gt[i], output[i]);
         }
     }
 
@@ -203,8 +218,70 @@ mod tests {
 
         // Copy the result of the kernel to the host.
         output_device.copy_to_host(&mut output[..]);
-        for (a, b) in gt.iter().zip(output.iter()) {
-            assert_eq!(*a, *b);
+        for i in 0..n {
+            assert_eq!(gt[i], output[i]);
+        }
+    }
+
+    #[test]
+    fn test_hash_gpu() {
+        // Setup the random number generator.
+        let mut rng = thread_rng();
+
+        // Setup the testing parameters.
+        let n = 128;
+        const N_INPUT: usize = 107;
+        let threads_per_block = 32;
+        let num_blocks = n / threads_per_block + 1;
+
+        // Generate the input data on the host.
+        let (external_round_constants, internal_round_constants) = round_constants();
+        let input = (0..n)
+            .flat_map(|_| [rng.gen::<BabyBear>(); N_INPUT].to_vec())
+            .collect::<Vec<_>>();
+        let mut output: Vec<[BabyBear; DIGEST_WIDTH]> = Vec::new();
+        output.resize(n, [BabyBear::zero(); DIGEST_WIDTH]);
+
+        // Copy the input data to the device.
+        let input_device = input.to_device();
+        let output_device = output.to_device();
+        let external_rc_device = external_round_constants.to_device();
+        let internal_rc_device = internal_round_constants.to_device();
+
+        // Execute the source implementation.
+        let perm = perm();
+        let sponge: PaddingFreeSponge<
+            Poseidon2<BabyBear, Poseidon2ExternalMatrixGeneral, DiffusionMatrixBabyBear, 16, 7>,
+            WIDTH,
+            RATE,
+            DIGEST_WIDTH,
+        > = PaddingFreeSponge::new(perm);
+
+        let mut gt: Vec<[BabyBear; DIGEST_WIDTH]> = Vec::new();
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..n {
+            let data = input[i * N_INPUT..(i + 1) * N_INPUT].to_vec();
+            gt.push(sponge.hash_iter(data));
+        }
+
+        // Execute the kernel.
+        unsafe {
+            poseidon2_bb31_16_gpu::hash(
+                input_device.as_slice().raw(),
+                N_INPUT,
+                output_device.as_slice().raw(),
+                external_rc_device.as_slice().raw(),
+                internal_rc_device.as_slice().raw(),
+                n,
+                num_blocks,
+                threads_per_block,
+            );
+        }
+
+        // Copy the result of the kernel to the host.
+        output_device.copy_to_host(&mut output[..]);
+        for i in 0..n {
+            assert_eq!(gt[i], output[i]);
         }
     }
 }
