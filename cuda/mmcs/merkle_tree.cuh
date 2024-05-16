96def113912ab27fb7f483242343ee260d8e0c2a
John Guibas <jtguibas@gmail.com>
2024-05-16 18:07:19 +0000

    merkle_tree

--- cuda/mmcs/merkle_tree.cuh
+++ cuda/mmcs/merkle_tree.cuh
@@ -9,62 +9,85 @@ struct RowMajorMatrix {
     int height;
 };
 
-namespace mmcs {
-class FieldMerkleTree {
-   private:
-    poseidon2_bb31_16::Hasher hasher;
+extern "C" namespace merkle_tree_gpu {
+    using namespace poseidon2_bb31_16;
 
-   public:
-    FieldMerkleTree(poseidon2_bb31_16::Hasher hasher) : hasher(hasher) {
-        this->hasher = hasher;
+    extern "C" void firstDigestLayer(
+        DeviceSlice<RowMajorMatrix> tallestMatrices,
+        DeviceSlice<bb31_t[DIGEST_WIDTH]> digests,
+        DeviceSlice<bb31_t[WIDTH]> external_rc, DeviceSlice<bb31_t> internal_rc,
+        size_t n, size_t nBlocks, size_t nThreadsPerBlock) {
+        Hasher hasher = Hasher(external_rc, internal_rc);
+        merkle_tree_kernels::firstDigestLayer<<<nBlocks, nThreadsPerBlock>>>(
+            tallestMatrices, digests, hasher);
     }
 
-    __device__ void firstDigestLayer(
-        DeviceSlice<RowMajorMatrix> tallestMatrices,
-        DeviceSlice<bb31_t[poseidon2_bb31_16::DIGEST_WIDTH]> digests,
-        size_t nRow) {
+    extern "C" void compressAndInject(
+        DeviceSlice<bb31_t[DIGEST_WIDTH]> prevLayer,
+        DeviceSlice<RowMajorMatrix> matricesToInject,
+        DeviceSlice<bb31_t[DIGEST_WIDTH]> nextDigests,
+        DeviceSlice<bb31_t[WIDTH]> external_rc, DeviceSlice<bb31_t> internal_rc,
+        size_t n, size_t nBlocks, size_t nThreadsPerBlock) {
+        Hasher hasher = Hasher(external_rc, internal_rc);
+        merkle_tree_kernels::compressAndInject<<<nBlocks, nThreadsPerBlock>>>(
+            prevLayer, matricesToInject, nextDigests, hasher);
+    }
+}
+
+namespace merkle_tree_kernels {
+using namespace poseidon2_bb31_16;
+
+__global__ void firstDigestLayer(DeviceSlice<RowMajorMatrix> tallestMatrices,
+                                 DeviceSlice<bb31_t[DIGEST_WIDTH]> digests,
+                                 Hasher hasher) {
+    int rowIdx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if (rowIdx >= tallestMatrices[0].height) {
+        return;
+    }
+
+    poseidon2_bb31_16::HasherState state;
+    for (int i = 0; i < tallestMatrices.length; i++) {
+        DeviceSlice<bb31_t> row = tallestMatrices[i].data.slice(
+            tallestMatrices[i].width * rowIdx, tallestMatrices[i].width);
+        hasher.absorb(row, state);
+    }
+    hasher.finalize(state, digests[rowIdx]);
+}
+
+__global__ void compressAndInject(DeviceSlice<bb31_t[DIGEST_WIDTH]> prevLayer,
+                                  DeviceSlice<RowMajorMatrix> matricesToInject,
+                                  DeviceSlice<bb31_t[DIGEST_WIDTH]> nextDigests,
+                                  Hasher hasher) {
+    int rowIdx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if (rowIdx >= prevLayer.length / 2) {
+        return;
+    }
+
+    if (matricesToInject.length == 0) {
+        return;
+    }
+
+    size_t nextLen = matricesToInject[0].height;
+    size_t nextLenPadded = prevLayer.length / 2;
+
+    bb31_t defaultDigest[poseidon2_bb31_16::DIGEST_WIDTH] = {
+        bb31_t(0), bb31_t(0), bb31_t(0), bb31_t(0), bb31_t(0), bb31_t(0)};
+
+    bb31_t digest[poseidon2_bb31_16::DIGEST_WIDTH];
+    hasher.compress(prevLayer[rowIdx * 2], prevLayer[rowIdx * 2 + 1], digest);
+
+    if (rowIdx < nextLen) {
+        bb31_t tallestDigest[poseidon2_bb31_16::DIGEST_WIDTH];
         poseidon2_bb31_16::HasherState state;
-        for (int i = 0; i < tallestMatrices.length; i++) {
-            DeviceSlice<bb31_t> row = tallestMatrices[i].data.slice(
-                tallestMatrices[i].width * nRow, tallestMatrices[i].width);
+        for (int i = 0; i < matricesToInject.length; i++) {
+            DeviceSlice<bb31_t> row = matricesToInject[i].data.slice(
+                matricesToInject[i].width * rowIdx, matricesToInject[i].width);
             hasher.absorb(row, state);
         }
-        hasher.finalize(state, digests[nRow]);
+        hasher.finalize(state, tallestDigest);
+        hasher.compress(digest, tallestDigest, nextDigests[rowIdx]);
+    } else {
+        hasher.compress(digest, defaultDigest, nextDigests[rowIdx]);
     }
-
-    __device__ void compressAndInject(
-        DeviceSlice<bb31_t[poseidon2_bb31_16::DIGEST_WIDTH]> prevLayer,
-        DeviceSlice<RowMajorMatrix> matricesToInject,
-        DeviceSlice<bb31_t[poseidon2_bb31_16::DIGEST_WIDTH]> nextDigests,
-        size_t nRow) {
-        if (matricesToInject.length == 0) {
-            return;
-        }
-
-        size_t nextLen = matricesToInject[0].height;
-        size_t nextLenPadded = prevLayer.length / 2;
-
-        bb31_t defaultDigest[poseidon2_bb31_16::DIGEST_WIDTH] = {
-            bb31_t(0), bb31_t(0), bb31_t(0), bb31_t(0), bb31_t(0), bb31_t(0)};
-
-        bb31_t digest[poseidon2_bb31_16::DIGEST_WIDTH];
-        hasher.compress(prevLayer[nRow * 2], prevLayer[nRow * 2 + 1], digest);
-
-        if (nRow < nextLen) {
-            bb31_t tallestDigest[poseidon2_bb31_16::DIGEST_WIDTH];
-            poseidon2_bb31_16::HasherState state;
-            for (int i = 0; i < matricesToInject.length; i++) {
-                DeviceSlice<bb31_t> row = matricesToInject[i].data.slice(
-                    matricesToInject[i].width * nRow,
-                    matricesToInject[i].width);
-                hasher.absorb(row, state);
-            }
-            hasher.finalize(state, tallestDigest);
-            hasher.compress(digest, tallestDigest, nextDigests[nRow]);
-        } else {
-            hasher.compress(digest, defaultDigest, nextDigests[nRow]);
-        }
-    }
-};
-
-}  // namespace mmcs
+}
+}  // namespace merkle_tree_kernels
