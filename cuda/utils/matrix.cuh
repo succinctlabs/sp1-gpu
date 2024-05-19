1f6b79274b566755a63a2913ec7505fe18909f87
tamirhemo <tamirhmo@gmai.com>
2024-05-19 23:14:01 +0000

    rm import

--- cuda/utils/matrix.cuh
+++ cuda/utils/matrix.cuh
@@ -1,9 +1,6 @@
 #pragma once
 
 #include "../fields/bb31_t.cuh"
-#include <index.h>
-#include <equations.h>
-#include <enact.cu>
 
 struct RowMajorMatrix {
     bb31_t *values;
@@ -12,7 +9,5 @@ struct RowMajorMatrix {
 };
 
 
-extern "C" void transpose_inplace(RowMajorMatrix *matrix) {
-    inplace::transpose_fn(true, matrix->values, matrix->height, matrix->width);
-}
+
 
