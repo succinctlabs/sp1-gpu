972f827d3cf9315592d13d8af27990188d97fd4c
tamirhemo <tamirhmo@gmai.com>
2024-05-31 21:26:37 +0000

    perm trace computation

--- core/src/stark/permutation.rs
+++ core/src/stark/permutation.rs
@@ -1,6 +1,5 @@
-use p3_air::{PairCol, VirtualPairCol};
-use p3_baby_bear::BabyBear;
-use p3_field::Field;
+use p3_air::PairCol;
+use p3_field::{ExtensionField, Field, Powers};
 use sp1_core::lookup::Interaction;
 
 use crate::device::{
@@ -13,6 +12,24 @@ use crate::device::{
 pub struct PairColDevice {
     column_idx: usize,
     is_preprocessed: bool,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct HostInteractions<F: Field> {
+    pub values_ptr: Vec<usize>,
+    pub multiplicities_ptr: Vec<usize>,
+    pub values_col_weights_ptr: Vec<usize>,
+
+    pub values_col_weights: Vec<(PairColDevice, F)>,
+    pub values_constants: Vec<F>,
+
+    pub mult_col_weights: Vec<(PairColDevice, F)>,
+    pub mult_constants: Vec<F>,
+
+    pub arg_indices: Vec<usize>,
+    pub is_send: Vec<bool>,
+    pub num_interactions: usize,
 }
 
 #[derive(Debug)]
@@ -51,8 +68,21 @@ pub struct DeviceInteractionsView<'a, F: Field> {
     pub num_interactions: usize,
 }
 
-impl<F: Field> DeviceInteractions<F> {
-    pub fn from_p3(sends: &[Interaction<F>], receives: &[Interaction<F>]) -> Self {
+impl PairColDevice {
+    pub fn get<T>(&self, preprocessed: &[T], main: &[T]) -> T
+    where
+        T: Copy,
+    {
+        if self.is_preprocessed {
+            preprocessed[self.column_idx]
+        } else {
+            main[self.column_idx]
+        }
+    }
+}
+
+impl<F: Field> HostInteractions<F> {
+    pub fn new(sends: &[Interaction<F>], receives: &[Interaction<F>]) -> Self {
         let mut values_ptr = vec![];
         let mut values_col_weights_ptr = vec![];
         let mut multiplicities_ptr = vec![];
@@ -68,13 +98,15 @@ impl<F: Field> DeviceInteractions<F> {
         let mut curr_values_ptr = 0;
         let mut curr_values_col_weight_ptr = 0;
         let mut curr_mult_ptr = 0;
-        for interaction in sends {
+
+        let num_sends = sends.len();
+        for (i, interaction) in sends.iter().chain(receives).enumerate() {
             // Register the values
             values_ptr.push(curr_values_ptr);
             for value in interaction.values.iter() {
                 values_col_weights_ptr.push(curr_values_col_weight_ptr);
                 for (col, weight) in value.column_weights.iter() {
-                    let col = PairColDevice::from_p3(col);
+                    let col = PairColDevice::from(*col);
                     values_col_weights.push((col, *weight));
                     curr_values_col_weight_ptr += 1;
                 }
@@ -85,30 +117,117 @@ impl<F: Field> DeviceInteractions<F> {
             // Register the multiplicity values
             multiplicities_ptr.push(curr_mult_ptr);
             for (col, weight) in interaction.multiplicity.column_weights.iter() {
-                let col = PairColDevice::from_p3(col);
+                let col = PairColDevice::from(*col);
                 mult_col_weights.push((col, *weight));
                 curr_mult_ptr += 1;
             }
             mult_constants.push(interaction.multiplicity.constant);
 
             arg_indices.push(interaction.argument_index());
-            is_send.push(true);
+
+            if i < num_sends {
+                is_send.push(true);
+            } else {
+                is_send.push(false);
+            }
         }
 
+        values_col_weights_ptr.push(curr_values_col_weight_ptr);
+        values_ptr.push(curr_values_ptr);
+        multiplicities_ptr.push(curr_mult_ptr);
+
         Self {
-            values_ptr: values_ptr.to_device(),
-            values_col_weights_ptr: values_col_weights_ptr.to_device(),
-            multiplicities_ptr: multiplicities_ptr.to_device(),
-            values_col_weights: values_col_weights.to_device(),
-            values_constants: values_constants.to_device(),
-            mult_col_weights: mult_col_weights.to_device(),
-            mult_constants: mult_constants.to_device(),
-            arg_indices: arg_indices.to_device(),
-            is_send: is_send.to_device(),
+            values_ptr,
+            values_col_weights_ptr,
+            multiplicities_ptr,
+            values_col_weights,
+            values_constants,
+            mult_col_weights,
+            mult_constants,
+            arg_indices,
+            is_send,
             num_interactions,
         }
     }
 
+    pub fn to_device(&self) -> DeviceInteractions<F> {
+        DeviceInteractions {
+            values_ptr: self.values_ptr.to_device(),
+            values_col_weights_ptr: self.values_col_weights_ptr.to_device(),
+            multiplicities_ptr: self.multiplicities_ptr.to_device(),
+            values_col_weights: self.values_col_weights.to_device(),
+            values_constants: self.values_constants.to_device(),
+            mult_col_weights: self.mult_col_weights.to_device(),
+            mult_constants: self.mult_constants.to_device(),
+            arg_indices: self.arg_indices.to_device(),
+            is_send: self.is_send.to_device(),
+            num_interactions: self.num_interactions,
+        }
+    }
+
+    pub fn populate_permutation_row<EF: ExtensionField<F>>(
+        &self,
+        row: &mut [EF],
+        preprocessed_row: &[F],
+        main_row: &[F],
+        alpha: EF,
+        betas: Powers<EF>,
+        batch_size: usize,
+    ) where
+        F: Field,
+    {
+        for i in (0..self.num_interactions).step_by(batch_size) {
+            let mut value = EF::zero();
+            for j in 0..batch_size {
+                // Calculate the interaction index.
+                let index = i + j;
+
+                if index >= self.num_interactions {
+                    break;
+                }
+
+                // Initialize the denominator and beta powers.
+                let mut denominator = alpha;
+                let mut betas = betas.clone();
+
+                // Add argument index to the denominator.
+                let argument_index = self.arg_indices[index];
+                denominator += betas.next().unwrap() * EF::from_canonical_usize(argument_index);
+
+                // Add the interaction values.
+                for k in self.values_ptr[index]..self.values_ptr[index + 1] {
+                    let beta = betas.next().unwrap();
+                    let mut acc = self.values_constants[k];
+                    for l in self.values_col_weights_ptr[k]..self.values_col_weights_ptr[k + 1] {
+                        let (col, weight) = self.values_col_weights[l];
+                        acc += col.get(preprocessed_row, main_row) * weight;
+                    }
+                    denominator += beta * acc;
+                }
+
+                // Calculate the multiplicity values.
+                let is_send = self.is_send[index];
+                let mut mult = self.mult_constants[index];
+                for k in self.multiplicities_ptr[index]..self.multiplicities_ptr[index + 1] {
+                    let (col, weight) = self.mult_col_weights[k];
+                    mult += col.get(preprocessed_row, main_row) * weight;
+                }
+
+                if !is_send {
+                    mult = -mult;
+                }
+
+                // Add `mult/ denominator` to the sum.
+                value += EF::from_base(mult) / denominator;
+            }
+            // Assign the value to the row.
+            let row_index = i / batch_size;
+            row[row_index] = value;
+        }
+    }
+}
+
+impl<F: Field> DeviceInteractions<F> {
     pub fn view(&self) -> DeviceInteractionsView<'_, F> {
         DeviceInteractionsView {
             values_ptr: self.values_ptr.as_slice(),
@@ -128,121 +247,99 @@ impl<F: Field> DeviceInteractions<F> {
     }
 }
 
-impl PairColDevice {
-    pub fn from_p3(p3_pair_col: &PairCol) -> Self {
-        match p3_pair_col {
-            PairCol::Preprocessed(p3_pair_col) => Self {
-                column_idx: *p3_pair_col,
+impl From<PairCol> for PairColDevice {
+    fn from(value: PairCol) -> Self {
+        match value {
+            PairCol::Preprocessed(column_idx) => Self {
+                column_idx,
                 is_preprocessed: true,
             },
-            PairCol::Main(p3_pair_col) => Self {
-                column_idx: *p3_pair_col,
+            PairCol::Main(column_idx) => Self {
+                column_idx,
                 is_preprocessed: false,
             },
         }
     }
 }
 
-#[derive(Debug)]
-#[repr(C)]
-pub struct VirtualPairColDevice<F: Field> {
-    columns: DeviceBuffer<PairColDevice>,
-    weights: DeviceBuffer<F>,
-    constant: F,
-    n_columns: usize,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use p3_air::BaseAir;
+    use p3_baby_bear::BabyBear;
+    use p3_field::extension::BinomialExtensionField;
+    use p3_field::AbstractField;
+    use p3_matrix::{dense::RowMajorMatrix, Matrix};
+    use rand::thread_rng;
+    use rand::Rng;
 
-impl VirtualPairColDevice<BabyBear> {
-    pub fn from_p3(c: &VirtualPairCol<BabyBear>) -> Self {
-        let (columns, weights): (Vec<_>, Vec<_>) = c
-            .column_weights
-            .iter()
-            .map(|cw| (PairColDevice::from_p3(&cw.0), cw.1))
-            .unzip();
-        let constant = c.constant;
-        VirtualPairColDevice {
-            columns: columns.to_device(),
-            weights: weights.to_device(),
-            constant,
-            n_columns: columns.len(),
+    use sp1_core::stark::permutation_trace_width;
+    use sp1_core::utils::tests::FIBONACCI_ELF;
+
+    use sp1_core::{
+        air::MachineAir,
+        runtime::Program,
+        stark::{populate_permutation_row, ByteChip, Chip},
+    };
+
+    type F = BabyBear;
+    const D: usize = 4;
+    type EF = BinomialExtensionField<F, D>;
+
+    #[test]
+    fn test_populate_permutation_row() {
+        let mut rng = thread_rng();
+
+        let air = ByteChip::<F>::default();
+        let chip = Chip::new(air);
+
+        let program = Program::from(FIBONACCI_ELF);
+
+        let num_rows = 1 << 16;
+        let preprocessed_trace = chip.generate_preprocessed_trace(&program).unwrap();
+
+        // Generate a random trace.
+        let main_trace = RowMajorMatrix::<F>::rand(&mut rng, num_rows, chip.width());
+
+        // Get the host interactions.
+        let host_interactions = HostInteractions::new(chip.sends(), chip.receives());
+
+        // For every row, compute the permutation row and compare the values.
+
+        let batch_size = 2;
+        let perm_width =
+            permutation_trace_width(chip.sends().len() + chip.receives().len(), batch_size);
+        let alpha = rng.gen::<EF>();
+        let beta = rng.gen::<EF>();
+        for i in 0..num_rows {
+            let prep_row = preprocessed_trace.row_slice(i);
+            let main_row = main_trace.row_slice(i);
+
+            let mut expected_row = vec![EF::zero(); perm_width];
+            populate_permutation_row(
+                &mut expected_row,
+                &prep_row,
+                &main_row,
+                chip.sends(),
+                chip.receives(),
+                alpha,
+                beta.powers(),
+                batch_size,
+            );
+
+            let mut row = vec![EF::zero(); perm_width];
+            host_interactions.populate_permutation_row(
+                &mut row,
+                &prep_row,
+                &main_row,
+                alpha,
+                beta.powers(),
+                batch_size,
+            );
+
+            for (exp, val) in expected_row.iter().zip(row.iter()) {
+                assert_eq!(exp, val, "row {} mismatch", i);
+            }
         }
     }
-
-    pub fn view(&self) -> VirtualPairColViewDevice<BabyBear> {
-        VirtualPairColViewDevice {
-            columns: self.columns.as_ptr(),
-            weights: self.weights.as_ptr(),
-            constant: self.constant,
-            n_columns: self.columns.len(),
-        }
-    }
 }
-
-#[derive(Copy, Clone, Debug)]
-#[repr(C)]
-pub struct VirtualPairColViewDevice<F: Field> {
-    columns: *const PairColDevice,
-    weights: *const F,
-    constant: F,
-    n_columns: usize,
-}
-
-#[derive(Debug)]
-#[repr(C)]
-pub struct InteractionDevice<F: Field> {
-    pub values: DeviceBuffer<VirtualPairColViewDevice<F>>,
-    pub multiplicity: VirtualPairColDevice<F>,
-    pub kind: usize,
-}
-
-impl InteractionDevice<BabyBear> {
-    pub fn from_p3(c: &Interaction<BabyBear>) -> (Self, Vec<VirtualPairColDevice<BabyBear>>) {
-        let values = c
-            .values
-            .iter()
-            .map(VirtualPairColDevice::from_p3)
-            .collect::<Vec<_>>();
-        let values_view = values.iter().map(|v| v.view()).collect::<Vec<_>>();
-        let multiplicity = VirtualPairColDevice::from_p3(&c.multiplicity);
-        let kind = c.argument_index();
-        (
-            InteractionDevice {
-                values: values_view.to_device(),
-                multiplicity,
-                kind,
-            },
-            values,
-        )
-    }
-}
-
-#[derive(Debug)]
-pub struct InteractionViewDevice<F: Field> {
-    pub values: *const VirtualPairColViewDevice<F>,
-    pub n_values: usize,
-    pub multiplicity: VirtualPairColViewDevice<F>,
-    pub kind: usize,
-}
-
-// fn generate_permutation_trace(
-//     sends: &[Interaction<BabyBear>],
-//     receives: &[Interaction<BabyBear>],
-//     main: ColMajorMatrixDevice<BabyBear>,
-//     random_elements: &[BinomialExtensionField<BabyBear, 4>],
-//     batch_size: usize,
-// ) {
-//     let (sends_device, send_device_values): (Vec<_>, Vec<_>) =
-//         sends.iter().map(|s| InteractionDevice::from_p3(s)).unzip();
-
-//     let alphas = generate_interaction_rlc_elements(sends, receives, random_elements[0]);
-//     let beta = random_elements[1];
-
-//     let permutation_trace_width = permutation_trace_width(sends.len() + receives.len(), batch_size);
-//     let height = main.height();
-
-//     let mut permutation_trace = ColMajorMatrixDevice::new(
-//         vec![BinomialExtensionField::<BabyBear, 4>::zero(); permutation_trace_width * height]
-//             .to_device(),
-//         height,
-//     );
-// }
