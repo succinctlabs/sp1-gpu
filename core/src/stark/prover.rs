44b459b54e392c915e741053ed536b3c02551aea
tamirhemo <tamirhmo@gmai.com>
2024-06-04 19:49:06 +0000

    init

--- core/src/stark/prover.rs
+++ core/src/stark/prover.rs
@@ -1,1 +1,17 @@
+use sp1_core::stark::{MachineProof, StarkGenericConfig, StarkMachine, StarkProvingKey};
 
+pub trait GpuProverConfig {
+    type SC: StarkGenericConfig;
+    type Air;
+    type Runtime;
+}
+
+pub struct GpuProver<C: GpuProverConfig> {
+    machine: StarkMachine<C::SC, C::Air>,
+}
+
+impl<C: GpuProverConfig> GpuProver<C> {
+    pub fn prove(&self, pk: &StarkProvingKey<C::SC>) -> MachineProof<C::SC> {
+        todo!()
+    }
+}
