4a9a9a4a0b16c5652b3b13aff8aebd41611c6387
tamirhemo <tamirhmo@gmai.com>
2024-06-10 21:36:13 +0000

    unify files

--- core/src/stark/ffi.rs
+++ core/src/stark/ffi.rs
@@ -30,3 +30,36 @@ extern "C" {
         num_threads_per_block: usize,
     );
 }
+
+pub(super) mod quotient_gpu {
+    use crate::matrix::MatrixViewDevice;
+    use crate::stark::quotient::{LagrangeSelectorsView, TwoAdicMultiplicativeCosetDevice};
+    use air::operation::Operation;
+    use p3_baby_bear::BabyBear;
+    use p3_field::extension::BinomialExtensionField;
+
+    #[link_name = "quotient_gpu"]
+    #[allow(unused_attributes)]
+    extern "C" {
+        #[link_name = "computeValues"]
+        #[allow(unused)]
+        pub fn compute_values(
+            chip_id: usize,
+            eval_program: *const Operation,
+            eval_program_len: usize,
+            cumulative_sum: BinomialExtensionField<BabyBear, 4>,
+            trace_domain: TwoAdicMultiplicativeCosetDevice<BabyBear>,
+            quotient_domain: TwoAdicMultiplicativeCosetDevice<BabyBear>,
+            preprocessed_trace_on_quotient_domain: MatrixViewDevice<BabyBear>,
+            main_trace_on_quotient_domain: MatrixViewDevice<BabyBear>,
+            permutation_trace_on_quotient_domain: MatrixViewDevice<BabyBear>,
+            perm_challenges: *const BinomialExtensionField<BabyBear, 4>,
+            alpha: BinomialExtensionField<BabyBear, 4>,
+            public_values: *const BabyBear,
+            selectors: LagrangeSelectorsView<BabyBear>,
+            quotient_values: *mut BinomialExtensionField<BabyBear, 4>,
+            num_blocks: usize,
+            num_threads_per_block: usize,
+        );
+    }
+}
