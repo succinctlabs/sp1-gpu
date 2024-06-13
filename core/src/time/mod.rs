fb63c2ffe9d1b4b80aa404abd5496db7222ea23b
Tamir Hemo <tamirhmo@gmail.com>
2024-06-13 16:33:31 -0700

    feat: tracing (#30)
    * tracer
    
    * checkpoint
    
    * cuda timing
    
    ---------
    
    Co-authored-by: tamirhemo <tamirhmo@gmai.com>

--- core/src/time/mod.rs
+++ core/src/time/mod.rs
@@ -1,11 +1,17 @@
 use std::time::Duration;
 
+mod layer;
+pub use layer::*;
+
 use crate::{
     device::error::CudaError,
     runtime::{event::CudaEvent, ffi, stream::UnsafeCudaStream},
 };
 
 pub struct CudaInstant(pub(crate) CudaEvent);
+
+unsafe impl Send for CudaInstant {}
+unsafe impl Sync for CudaInstant {}
 
 impl CudaInstant {
     pub fn now() -> Result<Self, CudaError> {
@@ -25,4 +31,74 @@ impl CudaInstant {
         let s = ms as f64 * 1e-3;
         Ok(Duration::from_secs_f64(s))
     }
+}
+
+#[macro_export]
+macro_rules! timed {
+    ($level:expr, $msg:expr, $block:expr) => {{
+        let start = CudaInstant::now().unwrap();
+        let result = $block;
+        tracing::event!($level, "{}: {:?}", $msg, start.elapsed().unwrap());
+        result
+    }};
+}
+
+#[macro_export]
+macro_rules! timed_info {
+    ($msg:expr, $block:expr) => {{
+        let start = CudaInstant::now().unwrap();
+        let result = $block;
+        tracing::event!(
+            tracing::Level::INFO,
+            "{}: {:?}",
+            $msg,
+            start.elapsed().unwrap()
+        );
+        result
+    }};
+}
+
+#[macro_export]
+macro_rules! timed_debug {
+    ($msg:expr, $block:expr) => {{
+        let start = CudaInstant::now().unwrap();
+        let result = $block;
+        tracing::event!(
+            tracing::Level::DEBUG,
+            "{}, time: {:?}",
+            $msg,
+            start.elapsed().unwrap()
+        );
+        result
+    }};
+}
+
+#[macro_export]
+macro_rules! timed_warn {
+    ($msg:expr, $block:expr) => {{
+        let start = CudaInstant::now().unwrap();
+        let result = $block;
+        tracing::event!(
+            tracing::Level::WARN,
+            "{}, time: {:?}",
+            $msg,
+            start.elapsed().unwrap()
+        );
+        result
+    }};
+}
+
+#[macro_export]
+macro_rules! timed_error {
+    ($msg:expr, $block:expr) => {{
+        let start = CudaInstant::now().unwrap();
+        let result = $block;
+        tracing::event!(
+            tracing::Level::ERROR,
+            "{}, time: {:?}",
+            $msg,
+            start.elapsed().unwrap()
+        );
+        result
+    }};
 }
---
+++ core/src/utils/tracer.rs
@@ -0,0 +1,46 @@
+use std::sync::Once;
+
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+use crate::time::TimeLayer;
+
+static INIT: Once = Once::new();
+
+/// Initializes the tracing subscriber.
+///
+/// Set the `RUST_LOG` environment variable to be set to `info` or `debug`.
+#[allow(dead_code)]
+pub fn init_tracer() {
+    INIT.call_once(|| {
+        let default_filter = "off";
+        let env_filter = EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| EnvFilter::new(default_filter))
+            .add_directive("p3_keccak_air=off".parse().unwrap())
+            .add_directive("p3_fri=off".parse().unwrap())
+            .add_directive("p3_dft=off".parse().unwrap())
+            .add_directive("p3_challenger=off".parse().unwrap());
+
+        // if the TRACER_TYPE environment variable is set, use it to determine which logger to configure
+        // (tracing_forest or tracing_subscriber)
+        // otherwise, default to 'flat'
+        let logger_type = std::env::var("TRACER_TYPE").unwrap_or_else(|_| "flat".to_string());
+        match logger_type.as_str() {
+            "flat" => {
+                tracing_subscriber::fmt::Subscriber::builder()
+                    .compact()
+                    .with_file(false)
+                    .with_target(false)
+                    .with_thread_names(false)
+                    .with_env_filter(env_filter)
+                    .finish()
+                    .with(TimeLayer)
+                    .init();
+            }
+            _ => {
+                panic!("Invalid logger type: {}", logger_type);
+            }
+        }
+    });
+}
