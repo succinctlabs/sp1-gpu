8aefbe12d591190cbfa457c7935fa5920cedf55a
tamirhemo <tamirhmo@gmai.com>
2024-06-27 21:33:45 +0000

    no threading

--- core/src/fri/commit.rs
+++ core/src/fri/commit.rs
@@ -69,22 +69,13 @@ impl TwoAdicFriCommitter<BabyBear, [BabyBear; DIGEST_WIDTH]> {
     where
         Matrix: Send + Sync + Borrow<CudaSync<ColMajorMatrixDevice<BabyBear>>>,
     {
-        let lde_evaluations = thread::scope(|s| {
-            let lde_evaluations = evaluations
-                .iter()
-                .map(|(domain, matrix)| {
-                    s.spawn(|| {
-                        let matrix = matrix.borrow();
-                        CudaSync::new(self.encode(*domain, matrix, true).unwrap()).unwrap()
-                    })
-                })
-                .collect::<Vec<_>>();
-
-            lde_evaluations
-                .into_iter()
-                .map(|lde| lde.join().unwrap())
-                .collect::<Vec<_>>()
-        });
+        let lde_evaluations = evaluations
+            .iter()
+            .map(|(domain, matrix)| {
+                let matrix = matrix.borrow();
+                CudaSync::new(self.encode(*domain, matrix, true).unwrap()).unwrap()
+            })
+            .collect::<Vec<_>>();
 
         let tree_device = FieldMerkleTreeGpu::new(lde_evaluations);
         let root_device = tree_device.root().into();
