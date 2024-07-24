71b8fd8f17adf8ba11dd0936aa4f5b7173ad9d22
Ubuntu <ubuntu@ip-172-31-23-59.ec2.internal>
2024-07-24 03:39:46 +0000

    prover refactor

--- core/src/stark/mod.rs
+++ core/src/stark/mod.rs
@@ -2,13 +2,11 @@ pub(super) mod ffi;
 mod permutation;
 mod prover;
 mod quotient;
-mod trace;
 mod utils;
 
 pub use permutation::*;
 pub use prover::*;
 pub use quotient::*;
-pub use trace::*;
 pub use utils::*;
 
 use p3_baby_bear::BabyBear;
@@ -16,20 +14,20 @@ use sp1_core::{stark::StarkGenericConfig, utils::BabyBearPoseidon2};
 
 pub trait BabyBearPoseidon2Config:
     StarkGenericConfig<
-    Val = BabyBear,
-    Challenge = <BabyBearPoseidon2 as StarkGenericConfig>::Challenge,
-    Challenger = <BabyBearPoseidon2 as StarkGenericConfig>::Challenger,
-    Pcs = <BabyBearPoseidon2 as StarkGenericConfig>::Pcs,
->
-{
-}
-
-impl<SC> BabyBearPoseidon2Config for SC where
-    SC: StarkGenericConfig<
         Val = BabyBear,
         Challenge = <BabyBearPoseidon2 as StarkGenericConfig>::Challenge,
         Challenger = <BabyBearPoseidon2 as StarkGenericConfig>::Challenger,
         Pcs = <BabyBearPoseidon2 as StarkGenericConfig>::Pcs,
     >
+{
+}
+
+impl<SC> BabyBearPoseidon2Config for SC where
+    SC: StarkGenericConfig<
+            Val = BabyBear,
+            Challenge = <BabyBearPoseidon2 as StarkGenericConfig>::Challenge,
+            Challenger = <BabyBearPoseidon2 as StarkGenericConfig>::Challenger,
+            Pcs = <BabyBearPoseidon2 as StarkGenericConfig>::Pcs,
+        >
 {
 }
