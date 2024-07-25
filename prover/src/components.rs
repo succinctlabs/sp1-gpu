2b619a764dd5710eb51f1d372fde725ef60aa489
Ubuntu <ubuntu@ip-172-31-23-59.ec2.internal>
2024-07-25 05:15:24 +0000

    feat: fast recursion

--- prover/src/components.rs
+++ prover/src/components.rs
@@ -2,7 +2,7 @@ use moongate_core::stark::StarkGpuProver;
 use sp1_core::stark::{CpuProver, RiscvAir, StarkGenericConfig};
 
 use sp1_prover::{
-    components::SP1ProverComponents, CompressAir, CoreSC, InnerSC, OuterSC, ReduceAir, WrapAir,
+    components::SP1ProverComponents, CompressAir, CoreSC, InnerSC, OuterSC, ShrinkAir, WrapAir,
 };
 
 #[derive(Debug, Clone, Copy)]
@@ -10,7 +10,8 @@ pub struct GpuProverComponents;
 
 impl SP1ProverComponents for GpuProverComponents {
     type CoreProver = StarkGpuProver<CoreSC, RiscvAir<<CoreSC as StarkGenericConfig>::Val>>;
-    type CompressProver = StarkGpuProver<InnerSC, ReduceAir<<InnerSC as StarkGenericConfig>::Val>>;
-    type ShrinkProver = CpuProver<InnerSC, CompressAir<<InnerSC as StarkGenericConfig>::Val>>;
+    type CompressProver =
+        StarkGpuProver<InnerSC, CompressAir<<InnerSC as StarkGenericConfig>::Val>>;
+    type ShrinkProver = CpuProver<InnerSC, ShrinkAir<<InnerSC as StarkGenericConfig>::Val>>;
     type WrapProver = CpuProver<OuterSC, WrapAir<<OuterSC as StarkGenericConfig>::Val>>;
 }
