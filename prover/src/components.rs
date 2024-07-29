9322af35a283ab3461f22a95c4e2d5bd408718b9
tamirhemo <tamirhmo@gmail.com>
2024-07-29 19:55:02 +0000

    components

--- prover/src/components.rs
+++ prover/src/components.rs
@@ -1,8 +1,9 @@
+use moongate_core::poseidon2::bn254::DeviceHasherBn254;
 use moongate_core::stark::StarkGpuProver;
 use moongate_core::{
     merkle_tree::FieldMerkleTreeDeviceCommitter, poseidon2::baby_bear::DeviceHasherBabyBear,
 };
-use sp1_core::stark::{CpuProver, RiscvAir, StarkGenericConfig};
+use sp1_core::stark::{RiscvAir, StarkGenericConfig};
 
 use sp1_prover::{
     components::SP1ProverComponents, CompressAir, CoreSC, InnerSC, OuterSC, ShrinkAir, WrapAir,
@@ -27,5 +28,9 @@ impl SP1ProverComponents for GpuProverComponents {
         FieldMerkleTreeDeviceCommitter<DeviceHasherBabyBear>,
         ShrinkAir<<InnerSC as StarkGenericConfig>::Val>,
     >;
-    type WrapProver = CpuProver<OuterSC, WrapAir<<OuterSC as StarkGenericConfig>::Val>>;
+    type WrapProver = StarkGpuProver<
+        OuterSC,
+        FieldMerkleTreeDeviceCommitter<DeviceHasherBn254>,
+        WrapAir<<OuterSC as StarkGenericConfig>::Val>,
+    >;
 }
