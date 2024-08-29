0dbdcc5f943c0d89daa78c5fa074304bb5c0d00d
mattstam <codeguy0112358@gmail.com>
2024-08-29 14:49:36 -0700

    ooms

--- eval/src/main.rs
+++ eval/src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
+use moongate_prover::{components::GpuProverComponents, gpu_prover_opts};
 use sp1_eval::evaluate_performance;
-use sp1_prover::components::GpuProverComponents;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    evaluate_performance::<GpuProverComponents>().await
+    let opts = gpu_prover_opts();
+    evaluate_performance::<GpuProverComponents>(opts).await
 }
