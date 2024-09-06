0475e3c921144700ebd00165f59e60218318706d
tamirhemo <tamirhmo@gmail.com>
2024-09-06 15:23:12 +0000

    update branch

--- perf/src/main.rs
+++ perf/src/main.rs
@@ -21,6 +21,8 @@ struct Args {
     pub program: Program,
     #[arg(short, long, default_value = "telemetry")]
     pub trace: Trace,
+    #[arg(short, long, default_value = "1")]
+    pub repititions: usize,
 }
 
 #[derive(Clone, Debug, ValueEnum)]
@@ -77,14 +79,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = gpu_prover_opts();
 
     let mut measurements = vec![];
+    let repititions = args.repititions;
     for (name, elf) in named_programs {
-        let measurement = make_measurement(&prover, name, elf, opts);
-        println!("{}", measurement);
-        measurements.push(measurement);
-        // Make a second measurement to see affects of the cache.
-        // let measurement = make_measurement(&prover, name, elf, opts);
-        // println!("{}", measurement);
-        // measurements.push(measurement);
+        for _ in 0..repititions {
+            let measurement = make_measurement(&prover, name, elf, opts);
+            println!("{}", measurement);
+            measurements.push(measurement);
+        }
     }
 
     write_measurements_to_csv(&measurements, "measurements.csv")?;
