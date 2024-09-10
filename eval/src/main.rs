25f985ee23bf7fc6837a1851e5447d1620b44f8e
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2024-09-10 16:33:58 -0700

    update sp1-eval deps

--- eval/src/main.rs
+++ eval/src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use moongate_prover::{components::GpuProverComponents, gpu_prover_opts};
+use moongate_prover::{components::GpuProverComponents};
 use sp1_eval::evaluate_performance;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = gpu_prover_opts();
-    evaluate_performance::<GpuProverComponents>(opts).await
+    evaluate_performance::<GpuProverComponents>().await
 }
