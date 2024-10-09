453ae0c92c16ab200a7c5e4a6a34e7f908847fb2
tamirhemo <tamirhmo@gmail.com>
2024-10-09 22:24:51 +0000

    init

--- perf/run_s3.sh
+++ perf/run_s3.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Check if both arguments are provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <s3_path>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <s3_path> <core|compress|shrink|wrap>"
     exit 1
 fi
 
 s3_path=$1
-
 stage=$2
 
 # Download files from S3
