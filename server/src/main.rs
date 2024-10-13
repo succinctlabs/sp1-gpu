faa0eb38e3c6d5c293ff33758a69d7dd9211eb13
umadayal <uma.dayal@yahoo.com>
2024-10-13 03:33:22 +0000

    print panic message

--- server/src/main.rs
+++ server/src/main.rs
@@ -159,6 +159,7 @@ fn handle_panic(err: Box<dyn Any + Send + 'static>) -> Response<Full<Bytes>> {
     } else {
         "Unknown panic message".to_string()
     };
+    println!("panic: {}", details);
 
     let body = serde_json::json!({
         "error": {
