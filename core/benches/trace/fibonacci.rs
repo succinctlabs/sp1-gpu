d4f12f65ef0022dd8dd2f8afd25bc1f04aa3b2ba
Tej Qu Nair <tej.q.nair@gmail.com>
2024-10-18 15:59:40 -0700

    use longer fibonacci

--- core/benches/trace/fibonacci.rs
+++ core/benches/trace/fibonacci.rs
@@ -6,14 +6,17 @@ use once_cell::sync::Lazy;
 use p3_baby_bear::BabyBear;
 use p3_matrix::dense::RowMajorMatrix;
 use rayon::prelude::*;
-use sp1_core_executor::programs::tests::fibonacci_program;
 use sp1_core_executor::ExecutionRecord;
 use sp1_core_executor::Executor;
+use sp1_core_executor::Program;
 use sp1_core_machine::riscv::RiscvAir;
 use sp1_stark::air::MachineAir;
 use sp1_stark::baby_bear_poseidon2::BabyBearPoseidon2;
 use sp1_stark::SP1CoreOpts;
 use sp1_stark::StarkMachine;
+
+const FIBONACCI_ELF: &[u8] =
+    include_bytes!("../../../perf/programs/fibonacci/riscv32im-succinct-zkvm-elf");
 
 struct Env {
     record: ExecutionRecord,
@@ -21,7 +24,7 @@ struct Env {
 }
 
 static SHARD: Lazy<Env> = Lazy::new(|| {
-    let mut executor = Executor::new(fibonacci_program(), SP1CoreOpts::default());
+    let mut executor = Executor::new(Program::from(FIBONACCI_ELF).unwrap(), SP1CoreOpts::default());
     executor.run().unwrap();
     let record = executor.record;
 
