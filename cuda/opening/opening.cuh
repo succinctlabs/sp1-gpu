31f58103c9ef3ce7f783d1533ec5ef801eaeceef
Tamir Hemo <tamirhmo@gmail.com>
2024-11-05 10:19:12 -0800

    fix: opening stream same as main stream (#156)
    * comment
    
    * fix opening stream

--- cuda/opening/opening.cuh
+++ cuda/opening/opening.cuh
@@ -89,173 +89,6 @@ template<typename F, typename EF> __global__ void foldEvenOddKernel(
 
 }
 
-__global__ void computeInverseDenominatorsKernel(
-    size_t* invRowIndices,
-    size_t* numsRows,
-    size_t* logsNumRows,
-    bb31_t* shifts,
-    bb31_t* threadGeneratorPowers,
-    bb31_extension_t* points,
-    bb31_extension_t* invDenoms
-) {
-    size_t rowIdx = blockIdx.x * blockDim.x + threadIdx.x;
-    size_t pointIdx = blockIdx.y * blockDim.y + threadIdx.y;
-
-    bb31_t shift = shifts[pointIdx];
-    size_t numRows = numsRows[pointIdx];
-    size_t InvIdx = invRowIndices[pointIdx];
-
-    if (rowIdx >= numRows) {
-        return;
-    }
-
-    bb31_t generator = threadGeneratorPowers[pointIdx * blockDim.x + 1];
-    bb31_t blockGenerator = generator^(blockIdx.x * blockDim.x);
-    bb31_t genPower = blockGenerator * threadGeneratorPowers[pointIdx * blockDim.x + threadIdx.x];
-    bb31_t x = shift * genPower;
-
-    bb31_extension_t point = points[pointIdx];
-    bb31_extension_t diff = bb31_extension_t(x) - point;
-
-    size_t logNumRows = logsNumRows[pointIdx];
-    size_t bitrev = bit_rev(rowIdx, logNumRows); 
-
-    invDenoms[InvIdx + bitrev] = diff.reciprocal();
-}
-
-__global__ void interpolateCosetsKernel(
-    bb31_t** polysEvals,          
-    size_t* cosetHeights,                 
-    size_t* cosetLogHeights,             
-    bb31_t* shifts,                       
-    bb31_extension_t* points,
-    bb31_t* gValues,
-    bb31_extension_t * barycentricScalars,
-    bb31_extension_t* output
-) {
-    size_t index = blockIdx.x;                             
-    uint32_t row = threadIdx.y * blockDim.x + threadIdx.x; 
-    uint32_t rowStride = blockDim.x * blockDim.y;         
-
-    bb31_t* polyEvals = polysEvals[index];
-    size_t cosetHeight = cosetHeights[index];
-    size_t cosetLogHeight = cosetLogHeights[index];
-    bb31_t shift = shifts[index];
-    bb31_extension_t point = points[index];
-    bb31_t* gWarpPowers = gValues + index * blockDim.x;
-    bb31_extension_t barycentricScalar = barycentricScalars[index];
-
-    bb31_extension_t sum = bb31_extension_t::zero();
-
-    bb31_t g = gWarpPowers[1];
-    bb31_t gStride = g^rowStride; 
-    bb31_t gPowers_i = (g^(threadIdx.y * blockDim.x)) * gWarpPowers[threadIdx.x];
-    for (int i = row; i < cosetHeight; i += rowStride) { 
-        size_t rev = bit_rev(i, cosetLogHeight);
-        bb31_extension_t diff = point - shift * gPowers_i;
-        bb31_extension_t scale = gPowers_i * diff.reciprocal();
-        sum += scale * polyEvals[rev];
-        gPowers_i *= gStride;
-    }
-
-    extern __shared__ bb31_extension_t sdata[];
-    sdata[row] = sum;
-    __syncthreads();
-
-    int steps = 32;
-    if (row < steps) {
-        bb31_extension_t blockSum = bb31_extension_t::zero();
-        for (int i = row; i < rowStride; i+=steps) { 
-            blockSum += sdata[i];
-        }
-        sdata[row] = blockSum;
-        __syncwarp();
-
-        if (row == 0) {
-            blockSum = bb31_extension_t::zero();
-            for (int i = 0; i < steps; i++) { 
-                blockSum += sdata[i];
-            }
-            output[index] = blockSum * barycentricScalar;
-        }
-    }
-}
-
-
-__global__ void reducedOpeningsKernel(
-    Matrix<bb31_t>* mats,
-    size_t* logHeights,
-    bb31_extension_t* points,
-    size_t* invIndices,
-    bb31_extension_t* invDenoms,
-    bb31_extension_t alpha,
-    bb31_extension_t* alphaPowOffsets,
-    bb31_extension_t* openedValues,
-    size_t * openedValuesIndices,
-    bb31_extension_t* reducedOpenings
-) {
-    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-    size_t pointIdx = blockIdx.y * blockDim.y + threadIdx.y;
-
-    Matrix<bb31_t> matrix = mats[pointIdx];
-    size_t numRows = matrix.height;
-    if (idx >= numRows) return;
-
-    size_t invIdx = invIndices[pointIdx];
-    bb31_extension_t alphaPowOffset = alphaPowOffsets[pointIdx];
-    size_t openValuesIdx = openedValuesIndices[pointIdx];
-
-    bb31_extension_t rowSum = bb31_extension_t::zero();
-
-    bb31_extension_t alphaPower = bb31_extension_t::one();
-    for (size_t i = 0; i < matrix.width; i++) {
-        rowSum += (matrix.values[i * matrix.height + idx] - openedValues[openValuesIdx + i]) * alphaPower;
-        alphaPower *= alpha;
-    }
-    reducedOpenings[invIdx + idx] = invDenoms[invIdx + idx] * alphaPowOffset * rowSum;
-}
-
-const size_t BLOCK_DIM = 256;
-const size_t COARSE_FACTOR = 1;
-
-const size_t MAX_LOG_HEIGHT = 32;
-
-__global__ void ReduceSumKernel(
-        size_t* logHeights, 
-        size_t* invIndices,
-        bb31_extension_t* reducedOpenings, 
-        Matrix<bb31_t>* reducedLeaves,
-        size_t * heightIndices , size_t numPoints) {
-
-    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    bb31_extension_t evenSums[MAX_LOG_HEIGHT];
-    bb31_extension_t oddSums[MAX_LOG_HEIGHT];
-
-    for (size_t i = 0; i < MAX_LOG_HEIGHT; i++) {
-        evenSums[i] = bb31_extension_t::zero();
-        oddSums[i] = bb31_extension_t::zero();
-    }
-
-    for (size_t i =0; i< numPoints; i++) {
-        size_t logHeight = logHeights[i];
-        size_t invIdx = invIndices[i];
-        if ((2 * idx + 1) >= (1 << logHeight)) continue;
-        evenSums[logHeight] += reducedOpenings[invIdx + 2 * idx];
-        oddSums[logHeight]  += reducedOpenings[invIdx + 2 * idx + 1];
-    }
-
-    for (size_t h = 0; h < MAX_LOG_HEIGHT; h++) { 
-        size_t heightIdx = heightIndices[h];
-        Matrix<bb31_t> leafMatrix = reducedLeaves[heightIdx];
-
-        if (idx >= leafMatrix.height) continue;
-        for (size_t k = 0; k < bb31_extension_t::D; k++) {
-            leafMatrix.values[k * leafMatrix.height + idx] = evenSums[h].value[k];
-            leafMatrix.values[(k + bb31_extension_t::D) * leafMatrix.height + idx] = oddSums[h].value[k];
-      }
-    }
-} 
 
 __device__ size_t log2_ceil_usize(size_t x) {
     float log2_val = __log2f(static_cast<float>(x));
@@ -354,130 +187,11 @@ extern "C" void shiftedPowers(
     Matrix<bb31_t> output, 
     size_t n, 
     size_t numTheads,
-    size_t numBlocks) {
-    opening_kernels::shiftedPowersKernel<<<numBlocks, numTheads>>>(blockPowers, shift, output, n);
+    size_t numBlocks,
+    cudaStream_t stream) {
+    opening_kernels::shiftedPowersKernel<<<numBlocks, numTheads, 0, stream>>>(blockPowers, shift, output, n);
 }
 
-
-extern "C" void computeInverseDenominators(
-    size_t maxRows,
-    size_t numPoints,
-    size_t* invRowIndices,
-    size_t* numsRows,
-    size_t* logsNumRows,
-    bb31_t* shifts,
-    bb31_t* threadGeneratorPowers,
-    bb31_extension_t* points,
-    bb31_extension_t* invDenoms
-) {
-    size_t numThreads = MAX_THREADS;
-    size_t numBlocksX = (maxRows - 1) / numThreads + 1; 
-
-    dim3 blockDim(numThreads);
-    dim3 gridDim(numBlocksX, numPoints);
-
-    opening_kernels::computeInverseDenominatorsKernel<<<gridDim, blockDim>>>(
-        invRowIndices,
-        numsRows,
-        logsNumRows,
-        shifts,
-        threadGeneratorPowers,
-        points,
-        invDenoms
-    );
-}
-
-extern "C" void interpolateCosets(
-    bb31_t** polysEvals,
-    size_t numPolys,
-    size_t*  cosetHeights,
-    size_t * cosetLogHeights,
-    bb31_t* shifts,
-    bb31_extension_t* points,
-    bb31_extension_t* barycentricScalars,
-    bb31_t* gValues,
-    bb31_extension_t* output
-) {
-    dim3 stageGrid(numPolys);
-    dim3 stageBlock(32, 32);
-
-    opening_kernels::interpolateCosetsKernel<<<
-    stageGrid, 
-    stageBlock, 
-    sizeof(bb31_extension_t) * stageBlock.x * stageBlock.y>>>(
-        polysEvals,
-        cosetHeights,
-        cosetLogHeights,
-        shifts,
-        points,
-        gValues,
-        barycentricScalars,
-        output
-    );
-}
-
-
-extern "C" void computeReducedOpenings(
-    Matrix<bb31_t>* mats,
-    size_t* logHeights,
-    size_t maxHeight,
-    bb31_extension_t* points,
-    size_t numPoints,
-    size_t * invIndices,
-    bb31_extension_t* invDenoms,
-    bb31_extension_t alpha,
-    bb31_extension_t* alphaPowOffsets,
-    bb31_extension_t * openedValues,
-    size_t * openedValuesIndices,
-    bb31_extension_t* reducedOpenings
-) {
-    size_t numThreads = MAX_THREADS;
-    size_t numBlocksX = (maxHeight - 1) / numThreads + 1; 
-
-    dim3 blockDim(numThreads);
-    dim3 gridDim(numBlocksX, numPoints);
-
-    opening_kernels::reducedOpeningsKernel<<<gridDim, blockDim>>>(
-        mats,
-        logHeights,
-        points,
-        invIndices,
-        invDenoms,
-        alpha,
-        alphaPowOffsets,
-        openedValues,
-        openedValuesIndices,
-        reducedOpenings
-    );
-}
-
-extern "C" size_t numBlocksSums(size_t maxHeight) {
-    size_t numThreads = opening_kernels::BLOCK_DIM;
-    size_t numBlocksX = ((maxHeight - 1) / (numThreads * opening_kernels::COARSE_FACTOR * 2)) + 1; 
-    return numBlocksX;
-}
-
-extern "C" void ReduceSums(
-    size_t* logHeights,
-    size_t maxHeight,
-    size_t * invIndices,
-    bb31_extension_t* reducedOpenings,
-    Matrix<bb31_t>* reducedLeaves,
-    size_t * heightIndices,
-    size_t numPoints
-) {
-    size_t numThreads = 512;
-    size_t numBlocks = ((maxHeight / 2 ) - 1) / numThreads + 1;
-
-    opening_kernels::ReduceSumKernel<<<numBlocks, numThreads>>>(
-        logHeights,
-        invIndices,
-        reducedOpenings,
-        reducedLeaves,
-        heightIndices,
-        numPoints
-    );
-}
 
 extern "C" void calculateOpenings(
     Matrix<bb31_t> *matrix_ptr,
@@ -489,7 +203,8 @@ extern "C" void calculateOpenings(
     size_t total_indices,
     size_t log_max_height,
     bool is_answering,
-    bb31_t* output
+    bb31_t* output,
+    cudaStream_t stream
 ) {
     // The idea of balancing thread count in blockDim based on 
     // min and max possible amount of indices, matrices and width.
@@ -506,7 +221,7 @@ extern "C" void calculateOpenings(
         (max_width      - 1) / blockDim.z + 1
     );
 
-    opening_kernels::calculateOpenings<<<gridDim, blockDim>>>(
+    opening_kernels::calculateOpenings<<<gridDim, blockDim, 0, stream>>>(
         matrix_ptr,
         width_offsets,
         query_indices,
@@ -530,7 +245,8 @@ extern "C" void calculateProof(
     void ***digests,
     void **output,  
     bool is_answering,
-    size_t field_id
+    size_t field_id,
+    cudaStream_t stream
 ) {
     dim3 blockDim(
         std::min(total_indices,         static_cast<size_t>(32)),
@@ -547,7 +263,7 @@ extern "C" void calculateProof(
     if (field_id == 0) {
         auto typed_digests = reinterpret_cast<poseidon2_bb31_16::BabyBear::F_t (**)[poseidon2_bb31_16::BabyBear::DIGEST_WIDTH]>(digests);
         auto typed_output = reinterpret_cast<poseidon2_bb31_16::BabyBear::F_t (*)[poseidon2_bb31_16::BabyBear::DIGEST_WIDTH]>(output);
-        opening_kernels::calculateProof<poseidon2_bb31_16::BabyBear><<<gridDim, blockDim>>>(
+        opening_kernels::calculateProof<poseidon2_bb31_16::BabyBear><<<gridDim, blockDim, 0, stream>>>(
             query_indices,
             log_max_heights,
             offset,
@@ -564,7 +280,7 @@ extern "C" void calculateProof(
     else if (field_id == 1) {
         auto typed_digests = reinterpret_cast<poseidon2_bn254_3::Bn254::F_t (**)[poseidon2_bn254_3::Bn254::DIGEST_WIDTH]>(digests);
         auto typed_output = reinterpret_cast<poseidon2_bn254_3::Bn254::F_t (*)[poseidon2_bn254_3::Bn254::DIGEST_WIDTH]>(output);
-        opening_kernels::calculateProof<poseidon2_bn254_3::Bn254><<<gridDim, blockDim>>>(
+        opening_kernels::calculateProof<poseidon2_bn254_3::Bn254><<<gridDim, blockDim, 0, stream>>>(
             query_indices,
             log_max_heights,
             offset,
@@ -583,19 +299,6 @@ extern "C" void calculateProof(
     }
 }
 
-extern "C" void batchMultiplicativeInverse(
-    bb31_extension_t* input,
-    bb31_extension_t* output,
-    size_t numElements
-) {
-    size_t numThreads = MAX_THREADS;
-    size_t numBlocks = numElements / numThreads + 1;
-    opening_kernels::batchMultiplicativeInverse<<<numBlocks, numThreads>>>(
-        input,
-        output,
-        numElements
-    );
-}
 
 extern "C" void foldEvenOdd(
     Matrix<bb31_t> evaluations,
@@ -603,12 +306,13 @@ extern "C" void foldEvenOdd(
     Matrix<bb31_t> output,
     Matrix<bb31_t> powers,
     bb31_t oneHalf,
-    bool inputExists
+    bool inputExists,
+    cudaStream_t stream
 ) {
     size_t numThreads = MAX_THREADS;
     size_t numBlocks = (output.height - 1) / numThreads + 1;
 
-    opening_kernels::foldEvenOddKernel<bb31_t, bb31_extension_t><<<numBlocks, numThreads>>>(
+    opening_kernels::foldEvenOddKernel<bb31_t, bb31_extension_t><<<numBlocks, numThreads, 0, stream>>>(
         evaluations,
         inputLeaves,
         output,
