a9c0e0ed6b65e5f5712453e405a413abfb4a1055
Chris Tian <chris@succinct.xyz>
2024-11-09 02:34:23 +0000

    nvcc flags

--- core/build.rs
+++ core/build.rs
@@ -41,6 +41,8 @@ fn main() {
         // nvcc.flag("--threads").flag("14");
         nvcc.flag("-lnvToolsExt");
         nvcc.flag("-ldl");
+        nvcc.flag("-arch=compute_89");
+        nvcc.flag("-code=sm_89");
 
         env::set_var("DEP_SPPARK_ROOT", "../sppark");
         if let Some(include) = env::var_os("DEP_SPPARK_ROOT") {
