bfe21cfa5dd9e7f55f4b97452d6780289cacebeb
jtguibas <jtguibas@gmail.com>
2024-11-11 08:07:03 +0000

    push

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -11,7 +11,7 @@ pub type SP1GpuProver = SP1Prover<GpuProverComponents>;
 
 const SHARD_MEM_RATIO: f64 = (1 << 21) as f64 / (23.0 * 1e9);
 const DEFFERRED_SPLIT_LOG_RATIO: usize = 4;
-const MAX_SHARD_SIZE: usize = 1 << 21;
+const MAX_SHARD_SIZE: usize = 1 << 22;
 
 pub fn gpu_prover_opts() -> SP1ProverOpts {
     let mut opts = SP1ProverOpts::default();
@@ -38,8 +38,8 @@ pub fn gpu_prover_opts() -> SP1ProverOpts {
     tracing::info!("Deffered split threshold set to {}", deferred_split_threshold);
     opts.core_opts.split_opts = SplitOpts::new(deferred_split_threshold);
 
-    opts.core_opts.records_and_traces_channel_capacity = 4;
-    opts.core_opts.trace_gen_workers = 4;
+    opts.core_opts.records_and_traces_channel_capacity = 6;
+    opts.core_opts.trace_gen_workers = 6;
 
     opts.recursion_opts.shard_batch_size = 1;
 
