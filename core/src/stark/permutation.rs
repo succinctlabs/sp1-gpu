bdee9aa7e2c26c710ccfa81a2e34f1a9c10645ae
rkm0959 <rkm0959@gmail.com>
2024-11-15 23:06:14 +0000

    fix global cumulative sum bug

--- core/src/stark/permutation.rs
+++ core/src/stark/permutation.rs
@@ -109,7 +109,7 @@ where
 
             let y = SepticExtension::<BabyBear>::from_base_fn(|i| {
                 let index =
-                    (main_trace.width() - 14 + i) * main_trace.height() + main_trace.height() - 1;
+                    (main_trace.width() - 7 + i) * main_trace.height() + main_trace.height() - 1;
                 let val = main_trace.values[index..index + 1].as_host_vec(main_trace.stream());
                 val[0]
             });
