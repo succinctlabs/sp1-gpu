07cb1d5b9c593a7186f86d564edbfb0d6d3488fb
Gautham Elango <gautham@succinct.xyz>
2024-11-21 23:07:05 +0000

    tracegen

--- core/src/tracegen/ffi.rs
+++ core/src/tracegen/ffi.rs
@@ -2,7 +2,7 @@ use crate::cuda_runtime::stream::CudaStreamHandle;
 use crate::matrix::MatrixViewMutDevice;
 use p3_baby_bear::BabyBear;
 use sp1_core_executor::events::AluEvent;
-use sp1_recursion_core::BaseAluEvent;
+use sp1_recursion_core::{BaseAluEvent, ExtAluEvent};
 
 /// cbindgen:ignore
 #[allow(unused_attributes)]
@@ -23,6 +23,13 @@ extern "C" {
     pub fn recursion_base_alu_generate_trace(
         trace: MatrixViewMutDevice<BabyBear>,
         events: *const BaseAluEvent<BabyBear>,
+        nb_events: u32,
+        stream: CudaStreamHandle,
+    );
+
+    pub fn recursion_ext_alu_generate_trace(
+        trace: MatrixViewMutDevice<BabyBear>,
+        events: *const ExtAluEvent<BabyBear>,
         nb_events: u32,
         stream: CudaStreamHandle,
     );
--- core/src/tracegen/recursion.rs
+++ core/src/tracegen/recursion.rs
@@ -1,16 +1,18 @@
-use p3_air::BaseAir;
-use p3_baby_bear::BabyBear;
-use sp1_core_machine::utils::next_power_of_two;
-use sp1_recursion_core::chips::alu_base::{BaseAluChip, NUM_BASE_ALU_ENTRIES_PER_ROW};
-
 use crate::{
     cuda_runtime::stream::CudaStream,
     device::{error::CudaError, memory::ToDevice},
     matrix::ColMajorMatrixDevice,
+    tracegen,
+};
+use p3_air::BaseAir;
+use p3_baby_bear::BabyBear;
+use sp1_core_machine::utils::next_power_of_two;
+use sp1_recursion_core::chips::{
+    alu_base::{BaseAluChip, NUM_BASE_ALU_ENTRIES_PER_ROW},
+    alu_ext::{ExtAluChip, NUM_EXT_ALU_ENTRIES_PER_ROW},
 };
 
 use super::DeviceAir;
-use crate::tracegen;
 
 impl DeviceAir<BabyBear> for BaseAluChip {
     fn generate_trace_device(
@@ -19,23 +21,16 @@ impl DeviceAir<BabyBear> for BaseAluChip {
         _: &mut Self::Record,
         stream: &CudaStream,
     ) -> Result<Option<ColMajorMatrixDevice<BabyBear>>, CudaError> {
-        // Get the events for the chip.
         let events = &input.base_alu_events;
-
-        // Copy the events to device.
         let events = events.to_device_async(stream)?;
 
-        // Get the number of rows.
         let nb_rows = self.num_rows(input).unwrap();
-
-        // Allocate the matrix.
         let mut trace = ColMajorMatrixDevice::<BabyBear>::with_capacity_in(
             <BaseAluChip as BaseAir<BabyBear>>::width(self),
             nb_rows,
             stream,
         )?;
 
-        // Generate the trace.
         unsafe {
             trace.set_max_width();
             tracegen::ffi::recursion_base_alu_generate_trace(
@@ -58,25 +53,85 @@ impl DeviceAir<BabyBear> for BaseAluChip {
     }
 }
 
+impl DeviceAir<BabyBear> for ExtAluChip {
+    fn generate_trace_device(
+        &self,
+        input: &Self::Record,
+        _: &mut Self::Record,
+        stream: &CudaStream,
+    ) -> Result<Option<ColMajorMatrixDevice<BabyBear>>, CudaError> {
+        let events = &input.ext_alu_events;
+        let events = events.to_device_async(stream)?;
+
+        let nb_rows = self.num_rows(input).unwrap();
+        let mut trace = ColMajorMatrixDevice::<BabyBear>::with_capacity_in(
+            <ExtAluChip as BaseAir<BabyBear>>::width(self),
+            nb_rows,
+            stream,
+        )?;
+
+        unsafe {
+            trace.set_max_width();
+            tracegen::ffi::recursion_ext_alu_generate_trace(
+                trace.view_mut(),
+                events.as_ptr(),
+                events.len() as u32,
+                stream.handle(),
+            );
+        }
+
+        Ok(Some(trace))
+    }
+
+    fn num_rows(&self, input: &Self::Record) -> Option<usize> {
+        let events = &input.base_alu_events;
+        Some(next_power_of_two(
+            events.len().div_ceil(NUM_EXT_ALU_ENTRIES_PER_ROW),
+            input.fixed_log2_rows(self),
+        ))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::cuda_runtime::stream::CudaStream;
+    use crate::tracegen::DeviceAir;
     use p3_baby_bear::BabyBear;
     use p3_field::AbstractField;
     use p3_matrix::dense::RowMajorMatrix;
-    use sp1_recursion_core::chips::alu_base::BaseAluChip;
-    use sp1_recursion_core::{BaseAluIo, ExecutionRecord};
+    use sp1_recursion_core::{BaseAluIo, ExecutionRecord, ExtAluIo};
     use sp1_stark::air::MachineAir;
 
-    use crate::tracegen::DeviceAir;
+    use super::*;
 
     #[test]
-    fn test_generate_trace_device_eq() {
+    fn test_base_alu() {
         type F = BabyBear;
 
         let chip = BaseAluChip;
         let shard = ExecutionRecord {
             base_alu_events: vec![BaseAluIo { out: F::one(), in1: F::one(), in2: F::one() }],
+            ..Default::default()
+        };
+        let trace: RowMajorMatrix<F> = chip.generate_trace(&shard, &mut ExecutionRecord::default());
+
+        let device_trace = chip
+            .generate_trace_device(&shard, &mut ExecutionRecord::default(), &CudaStream::default())
+            .unwrap()
+            .unwrap();
+        assert_eq!(trace, device_trace.to_host_naive());
+    }
+
+    #[test]
+    fn test_ext_alu() {
+        type F = BabyBear;
+
+        let chip = ExtAluChip;
+        let shard = ExecutionRecord {
+            ext_alu_events: vec![ExtAluIo {
+                out: F::one().into(),
+                in1: F::one().into(),
+                in2: F::one().into(),
+            }],
             ..Default::default()
         };
         let trace: RowMajorMatrix<F> = chip.generate_trace(&shard, &mut ExecutionRecord::default());
