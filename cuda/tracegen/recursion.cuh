5b3e1a31198003313a2f97613054d208bf586ab5
Gautham Elango <gautham@succinct.xyz>
2024-12-13 17:57:34 +0000

    fixes

--- cuda/tracegen/recursion.cuh
+++ cuda/tracegen/recursion.cuh
@@ -500,3 +500,51 @@ extern "C" rustCudaError_t recursion_poseidon2_wide_generate_trace(
 
     return CUDA_SUCCESS_MOON;
 }
+
+template<class T>
+__global__ void recursion_mem_variable_generate_trace_kernel(
+    MatrixViewMutDevice<T> trace,
+    const sp1_recursion_core_sys::MemIo<sp1_recursion_core_sys::Block<T>>*
+        events,
+    uintptr_t nb_events
+) {
+    static const size_t COLUMNS =
+        sizeof(sp1_recursion_core_sys::Block<T>) / sizeof(T);
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    for (; i < nb_events; i += blockDim.x * gridDim.x) {
+        sp1_recursion_core_sys::Block<T> cols;
+        sp1_recursion_core_sys::mem_variable::event_to_row<T>(events[i], cols);
+
+        const T* arr = reinterpret_cast<T*>(&cols);
+        for (size_t j = 0; j < COLUMNS; ++j) {
+            trace.values[i + j * trace.height] = arr[j];
+        }
+    }
+}
+
+extern "C" rustCudaError_t recursion_mem_variable_generate_trace(
+    MatrixViewMutDevice<bb31_t> trace,
+    const sp1_recursion_core_sys::MemIo<sp1_recursion_core_sys::Block<bb31_t>>*
+        events,
+    uintptr_t nb_events,
+    CudaStreamHandle stream_handle
+) {
+    const cudaStream_t stream = reinterpret_cast<cudaStream_t>(stream_handle);
+    CUDA_OK(cudaMemsetAsync(
+        trace.values,
+        0,
+        trace.width * trace.height * sizeof(bb31_t),
+        stream
+    ));
+
+    static const int M = 256;
+    recursion_mem_variable_generate_trace_kernel<bb31_t>
+        <<<(trace.height - 1) / M + 1, M, 0, stream>>>(
+            trace,
+            events,
+            nb_events
+        );
+
+    return CUDA_SUCCESS_MOON;
+}
