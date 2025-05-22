b1028878926efb364917f87757a2fb02f47349c4
Aurélien <3535019+leruaa@users.noreply.github.com>
2025-05-22 19:33:35 +0200

    Merge pull request #225 from succinctlabs/aurelien/gro2-550-fix-moongate-statefulness-issue
    feat: handle stateless proving request

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     any::Any,
-    sync::{Arc, Mutex},
+    collections::{hash_map::Entry, HashMap},
+    sync::{Arc, Mutex, RwLock},
 };
 
 use async_trait::async_trait;
@@ -12,12 +13,15 @@ use http::{
 use http_body_util::Full;
 use moongate_core::utils::init_tracer;
 use moongate_prover::{components::GpuProverComponents, gpu_prover_opts};
+use sha2::{Digest, Sha256};
 use sp1_core_executor::{Program, SP1Context};
+use sp1_core_machine::io::SP1Stdin;
 use sp1_cuda::{
     CompressRequestPayload, ProveCoreRequestPayload, SetupRequestPayload, SetupResponsePayload,
-    ShrinkRequestPayload, WrapRequestPayload,
+    ShrinkRequestPayload, StatelessProveCoreRequestPayload, WrapRequestPayload,
 };
 use sp1_prover::{DeviceProvingKey, SP1Prover};
+use sp1_stark::MachineProver;
 use tower_http::catch_panic::CatchPanicLayer;
 use twirp::{
     axum::{self},
@@ -31,10 +35,20 @@ use jemallocator::Jemalloc;
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
+struct ProvingContext {
+    program: Program,
+    pk_d: DeviceProvingKey<GpuProverComponents>,
+}
+
+impl ProvingContext {
+    pub fn new(program: Program, pk_d: DeviceProvingKey<GpuProverComponents>) -> Self {
+        Self { program, pk_d }
+    }
+}
+
 struct MoongateProverServer {
     prover: Arc<Mutex<Option<SP1Prover<GpuProverComponents>>>>,
-    pk: Arc<Mutex<Option<DeviceProvingKey<GpuProverComponents>>>>,
-    program: Arc<Mutex<Option<Program>>>,
+    proving_contexts: Arc<RwLock<HashMap<Vec<u8>, Mutex<ProvingContext>>>>,
 }
 
 impl MoongateProverServer {
@@ -42,8 +56,7 @@ impl MoongateProverServer {
     pub fn new() -> Self {
         let server = Self {
             prover: Arc::new(Mutex::new(None)),
-            pk: Arc::new(Mutex::new(None)),
-            program: Arc::new(Mutex::new(None)),
+            proving_contexts: Arc::new(RwLock::new(HashMap::new())),
         };
         server.init();
         server
@@ -59,6 +72,34 @@ impl MoongateProverServer {
             let mut prover_lock = prover_mutex.lock().unwrap();
             *prover_lock = Some(prover);
         });
+    }
+
+    fn prove_core_internal(
+        &self,
+        proving_context: &ProvingContext,
+        stdin: &SP1Stdin,
+    ) -> Result<sp1_cuda::proto::api::ProveCoreResponse, twirp::TwirpErrorResponse> {
+        let result = tracing::info_span!("prove core").in_scope(|| {
+            self.prover
+                .lock()
+                .unwrap()
+                .as_ref()
+                .ok_or_else(|| internal("prover not ready".to_string()))?
+                .prove_core(
+                    &proving_context.pk_d,
+                    proving_context.program.clone(),
+                    stdin,
+                    gpu_prover_opts(),
+                    SP1Context::default(),
+                )
+                .map_err(|e| internal(format!("failed to prove core {}", e)))
+        })?;
+
+        let result = tracing::info_span!("serialize proof result").in_scope(|| {
+            bincode::serialize(&result).map_err(|e| internal(format!("failed to serialize {}", e)))
+        })?;
+
+        Ok(sp1_cuda::proto::api::ProveCoreResponse { result })
     }
 }
 
@@ -89,10 +130,10 @@ impl sp1_cuda::proto::api::ProverService for MoongateProverServer {
                 .map(|prover| prover.setup(&payload.elf))
         })?;
 
-        let mut pk_ref = self.pk.lock().map_err(|e| internal(format!("{}", e)))?;
-        *pk_ref = Some(pk_d);
-        let mut program_ref = self.program.lock().map_err(|e| internal(format!("{}", e)))?;
-        *program_ref = Some(program.clone());
+        self.proving_contexts
+            .write()
+            .map_err(|e| internal(format!("{}", e)))?
+            .insert(elf_to_hash(&payload.elf), Mutex::new(ProvingContext::new(program, pk_d)));
 
         let response = SetupResponsePayload { pk, vk };
         let result = bincode::serialize(&response)
@@ -106,35 +147,77 @@ impl sp1_cuda::proto::api::ProverService for MoongateProverServer {
         _: twirp::Context,
         req: sp1_cuda::proto::api::ProveCoreRequest,
     ) -> Result<sp1_cuda::proto::api::ProveCoreResponse, twirp::TwirpErrorResponse> {
+        let proving_contexts =
+            self.proving_contexts.read().map_err(|e| internal(format!("{}", e)))?;
+
+        if proving_contexts.len() > 1 {
+            return Err(internal(
+                "stateless proving must be used when registering more than one program",
+            ));
+        }
+
+        let proving_context = proving_contexts
+            .iter()
+            .next()
+            .ok_or_else(|| internal("proving key and program not provided"))?
+            .1
+            .lock()
+            .map_err(|e| internal(format!("{}", e)))?;
+
         let payload: ProveCoreRequestPayload = tracing::info_span!("deserializing proof request")
             .in_scope(|| {
             bincode::deserialize(&req.data)
                 .map_err(|e| internal(format!("failed to deserialize {}", e)))
         })?;
 
-        let pk_ref = self.pk.lock().map_err(|e| internal(format!("{}", e)))?;
-        let pk = pk_ref.as_ref().ok_or_else(|| internal("proving key not provided".to_string()))?;
-        let program_ref = self.program.lock().map_err(|e| internal(format!("{}", e)))?;
-        let program = program_ref
-            .as_ref()
-            .ok_or_else(|| internal("program not provided".to_string()))?
-            .clone();
+        self.prove_core_internal(&proving_context, &payload.stdin)
+    }
 
-        let result = tracing::info_span!("prove core").in_scope(|| {
-            self.prover
-                .lock()
-                .unwrap()
-                .as_ref()
-                .ok_or_else(|| internal("prover not ready".to_string()))?
-                .prove_core(pk, program, &payload.stdin, gpu_prover_opts(), SP1Context::default())
-                .map_err(|e| internal(format!("failed to prove core {}", e)))
-        })?;
+    async fn prove_core_stateless(
+        &self,
+        _: twirp::Context,
+        req: sp1_cuda::proto::api::ProveCoreRequest,
+    ) -> Result<sp1_cuda::proto::api::ProveCoreResponse, twirp::TwirpErrorResponse> {
+        let mut proving_contexts =
+            self.proving_contexts.write().map_err(|e| internal(format!("{}", e)))?;
 
-        let result = tracing::info_span!("serialize proof result").in_scope(|| {
-            bincode::serialize(&result).map_err(|e| internal(format!("failed to serialize {}", e)))
-        })?;
+        let payload: StatelessProveCoreRequestPayload =
+            tracing::info_span!("deserializing proof request").in_scope(|| {
+                bincode::deserialize(&req.data)
+                    .map_err(|e| internal(format!("failed to deserialize {}", e)))
+            })?;
 
-        Ok(sp1_cuda::proto::api::ProveCoreResponse { result })
+        let proving_context = match proving_contexts.entry(elf_to_hash(&payload.pk.elf)) {
+            Entry::Occupied(occupied) => occupied.into_mut(),
+            Entry::Vacant(vacant) => {
+                let program = tracing::info_span!("get program").in_scope(|| {
+                    self.prover
+                        .lock()
+                        .unwrap()
+                        .as_ref()
+                        .ok_or_else(|| internal("prover not ready"))
+                        .and_then(|prover| {
+                            prover
+                                .get_program(&payload.pk.elf)
+                                .map_err(|err| internal(format!("{err}")))
+                        })
+                })?;
+                let pk_d = tracing::info_span!("pk to device").in_scope(|| {
+                    self.prover
+                        .lock()
+                        .unwrap()
+                        .as_ref()
+                        .ok_or_else(|| internal("prover not ready"))
+                        .map(|prover| prover.core_prover.pk_to_device(&payload.pk.pk))
+                })?;
+
+                vacant.insert(Mutex::new(ProvingContext::new(program, pk_d)))
+            }
+        };
+
+        let proving_context = proving_context.get_mut().map_err(|e| internal(format!("{}", e)))?;
+
+        self.prove_core_internal(proving_context, &payload.stdin)
     }
 
     async fn compress(
@@ -251,4 +334,8 @@ pub async fn main() {
     if let Err(e) = axum::serve(tcp_listener, app).await {
         eprintln!("server error: {}", e);
     }
+}
+
+fn elf_to_hash(elf: &[u8]) -> Vec<u8> {
+    Sha256::digest(elf).to_vec()
 }
