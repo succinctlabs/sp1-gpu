cc9801f69aa894fb41e304024aa265c839910f8b
jtguibas <john@succinct.xyz>
2025-06-05 11:18:13 -0700

    new docker image for 5090s

--- core/build.rs
+++ core/build.rs
@@ -188,6 +188,7 @@ fn main() {
         cc_builder.flag("--expt-relaxed-constexpr");
         cc_builder.flag("-gencode=arch=compute_86,code=sm_86");
         cc_builder.flag("-gencode=arch=compute_89,code=sm_89");
+        cc_builder.flag("--gencode=arch=compute_120,code=sm_120");
 
         env::set_var("DEP_SPPARK_ROOT", "../sppark");
         if let Some(include) = env::var_os("DEP_SPPARK_ROOT") {
